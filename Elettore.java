82496574d7a39e00d9f6403115a88b9eec6435e4
sataraja <90857297+sataraja@users.noreply.github.com>
2021-09-16 18:35:53 +0200

    Add files via upload

---
+++ Elettore.java
@@ -0,0 +1,14 @@
+package ELETTORE;
+
+public class Elettore {
+
+	public TipoElettore tipo;
+	public String comune;
+	
+	
+	public Elettore(String comune, TipoElettore tipo) {
+		this.tipo = tipo;
+		this.comune=comune;
+	}
+	
+}
